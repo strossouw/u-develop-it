2c415956af7c86fe171dffcf01f068f83fd33c4d
SRoss <srossouw@uwm.edu>
2022-08-10 21:16:49 -0500

    add Heroku

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const db = mysql.createConnection(
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: '',
+    password: 'Mason@89Snowy54909',
     database: 'election'
   },
   console.log('Connected to the election database.')
