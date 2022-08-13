314a1197c3e7202bc1869d928d1bf816b770518b
SRoss <srossouw@uwm.edu>
2022-08-13 09:30:12 -0500

    finished module

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql2");
+
+
+// Connect to database
+const db = mysql.createConnection({
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password 
+    password: 'Mason@89Snowy54909',
+    database: 'election'
+  });
+
+  module.exports = db;
